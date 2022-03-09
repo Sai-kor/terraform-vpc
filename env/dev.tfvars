bacf32d4a9375043ed7a9298ca56b5cd32cebea7
sai k <psai0073@gmail.com>
2022-03-08 22:26:14 -0500

    route table tf file

--- env/dev.tfvars
+++ env/dev.tfvars
@@ -1,4 +1,4 @@
 VPC_CIDR = "10.0.0.0/16"
 ENV = "dev"
-PUBLIC_SUBNET_CIDR = ["10.0.0.0/24","10.0.1.0/24"]
+PUBLIC_SUBNET_CIDR = ["10.0.0.0/24","10.0.1.0/24"] // if a 3rd subnet is given it picks 1c AZ and then 1d and goes on and nerver error out as a default functionality in terraform
 PRIVATE_SUBNET_CIDR = ["10.0.100.0/24","10.0.101.0/24"] //chosen wide different range between these two
---
+++ route-tables.tf
@@ -0,0 +1,28 @@
+resource "aws_route_table" "public-table" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "public-rt"
+    ENV = var.ENV
+  }
+}
+
+resource "aws_route_table" "private-table" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "private-rt"
+    ENV = var.ENV
+  }
+}
+
+
+#route {
+#  cidr_block = "10.0.1.0/24"
+#  gateway_id = aws_internet_gateway.example.id
+#}
+#
+#route {
+#  ipv6_cidr_block        = "::/0"
+#  egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
+#}

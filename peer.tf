b16f43e3df2182084842ea27cf6ff61608f8452a
sai k <psai0073@gmail.com>
2022-03-09 00:17:27 -0500

    peer con & data file

--- peer.tf
+++ peer.tf
@@ -1,6 +1,6 @@
-resource "aws_vpc_peering_connection" "foo" {
+resource "aws_vpc_peering_connection" "peer-connection" {
   peer_owner_id = data.aws_caller_identity.current.account_id //gets info from data.tf datasource
   peer_vpc_id   = aws_vpc.main.id
-  vpc_id        = aws_vpc.foo.id
-  auto_accept   = true // peering connection requires to accept the connection rather than doing it manually we use auto accept
+  vpc_id        = data.aws_vpc.default.id
+  auto_accept   = true       // peering connection requires to accept the connection rather than doing it manually we use auto accept
 }

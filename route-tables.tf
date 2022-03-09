6692b0f2a5c14f2741bdcaf92db9f0273df836fe
sai k <psai0073@gmail.com>
2022-03-09 00:28:44 -0500

    default rt table

--- route-tables.tf
+++ route-tables.tf
@@ -51,6 +51,12 @@ resource "aws_route" "private-peer-route" {
   destination_cidr_block    = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peer-connection.id
 }
+
+resource "aws_route" "default-table-route" {
+  route_table_id            = data.aws_vpc.default.main_route_table_id
+  destination_cidr_block    = var.VPC_CIDR
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer-connection.id
+}
 #route {
 #  cidr_block = "10.0.1.0/24"
 #  gateway_id = aws_internet_gateway.example.id

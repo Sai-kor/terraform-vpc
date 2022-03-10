48c593d882ae1170ff4ac52e7962ff882e42bab6
sai k <psai0073@gmail.com>
2022-03-09 20:22:03 -0500

    route 53 dns hosted zone add vpd and enable dns options in vpc

--- vpc.tf
+++ vpc.tf
@@ -2,6 +2,8 @@
 
 resource "aws_vpc" "main" {
   cidr_block       = var.VPC_CIDR //dynamic based on the env
+  enable_dns_hostnames = true
+  enable_dns_support = true // these 2 dns should be enabled for vpc to work with dns hosted zone
   tags = {
     Name = var.ENV
   }

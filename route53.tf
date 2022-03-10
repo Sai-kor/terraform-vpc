ee3a1ce71179725b65001e86f422c16e831eb8bc
sai k <psai0073@gmail.com>
2022-03-09 20:47:22 -0500

    changed resouce of route53 file and name privqate dns

--- route53.tf
+++ route53.tf
@@ -1,4 +1,4 @@
-resource "aws_route53_vpc_association_authorization" "vpc-route53-hosted-zone" {
+resource "aws_route53_zone_association" "private-dns" {
   vpc_id  = aws_vpc.main.id
   zone_id = var.PRIVATE_HOSTED_ZONE_ID
 }
